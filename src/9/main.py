f5e277d38ca6c96d5429aeb1fe8e53a03044565d
Ajay Chani <ajay.chani@gmail.com>
2024-12-15 23:07:35 +0100

    Refactor

--- src/9/main.py
+++ src/9/main.py
@@ -1,4 +1,3 @@
-
 def get_available_spaces():
   available_spaces, file_locations, curr_index, file_id = [], [], 0, 0
   for i, segment_size in enumerate(disk_map):
@@ -9,6 +8,10 @@ def get_available_spaces():
       available_spaces.append((curr_index,segment_size))
     curr_index += disk_map[i]
   return available_spaces, file_locations
+
+
+def get_checksum(file_id, start_index, block_count):
+  return file_id * ((start_index - 1)*block_count + (block_count * (block_count+1))//2)
 
 
 def solve_part2():
@@ -33,7 +36,7 @@ def solve_part2():
 
   ans = 0
   for file_id, file_start_index, file_size in new_file_locations:
-    ans +=  file_id * ((file_start_index - 1)*file_size + (file_size * (file_size+1))//2)
+    ans += get_checksum(file_id, file_start_index, file_size)
   print(f"Part 2: {ans}")
     
 
@@ -47,7 +50,7 @@ def solve_part1():
     if curr_seg % 2 == 0: 
       file_id = curr_seg // 2
       blocks_processed = curr_seg_size
-      ans += file_id * ((curr_index - 1)*blocks_processed + (blocks_processed * (blocks_processed+1))//2)
+      ans += get_checksum(file_id, curr_index, blocks_processed)
       curr_index += blocks_processed
       curr_seg += 1
       curr_seg_size = disk_map[curr_seg]
@@ -62,7 +65,7 @@ def solve_part1():
         blocks_processed = spaces_available
         blocks_remaining_from_current_file -= spaces_available
         spaces_available = 0 
-      ans += file_id * ((curr_index - 1)*blocks_processed + (blocks_processed * (blocks_processed+1))//2)
+      ans += get_checksum(file_id, curr_index, blocks_processed)
       curr_index += blocks_processed
       if spaces_available == 0: 
         curr_seg += 1
@@ -72,7 +75,7 @@ def solve_part1():
         
   if disk_map[next_file_to_move] > blocks_remaining_from_current_file > 0 : 
     blocks_processed = blocks_remaining_from_current_file
-    ans += file_id * ((curr_index - 1)*blocks_processed + (blocks_processed * (blocks_processed+1))//2)
+    ans += get_checksum(file_id, curr_index, blocks_processed)
   print(f"Part 1: {ans}" )  
 
 
